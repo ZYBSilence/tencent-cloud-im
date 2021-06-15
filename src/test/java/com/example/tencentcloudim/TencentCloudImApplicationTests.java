f91e2ce61b7ae279fd6bada66ecb28069ea28199
silence <13939420217@163.com>
2021-06-15 11:39:35 +0800

    feat: 删除多余代码，更改测试类
    1.删除多余代码；
    2.更改测试类；

--- src/test/java/com/example/tencentcloudim/TencentCloudImApplicationTests.java
+++ src/test/java/com/example/tencentcloudim/TencentCloudImApplicationTests.java
@@ -1,8 +1,6 @@
 package com.example.tencentcloudim;
 
-import com.example.tencentcloudim.config.TencentCloudImConfig;
-import com.example.tencentcloudim.constant.TencentCloudImApiConstant;
-import org.apache.commons.lang3.RandomUtils;
+import com.example.tencentcloudim.util.TencentCloudImUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,13 +9,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TencentCloudImApplicationTests {
     @Autowired
-    private TencentCloudImConfig tencentCloudImConfig;
+    private TencentCloudImUtil tencentCloudImUtil;
     @Test
     void contextLoads() {
-        Integer random = RandomUtils.nextInt(0, 999999999);
-        String aaa = tencentCloudImConfig.getTxCloudUserSig("aaa");
-//        String httpsUrl = TencentCloudImConfig.getHttpsUrl(TencentCloudImApiConstant.AccountManage.ACCOUNT_CHECK, TencentCloudImConfig.getTxCloudUserSig("administrator"), random);
-//        System.out.println(httpsUrl);
+        String userSig = tencentCloudImUtil.getTxCloudUserSig();
+        System.out.println(userSig);
     }
 
 }
